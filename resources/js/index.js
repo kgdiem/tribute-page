8d5e90ea9d071b4b0cac06ab1656fab368407c89
Kevin <kevin@Kevins-MBP.fios-router.home>
2017-10-28 11:06:04 -0400

    lazy load font

---
+++ resources/js/index.js
@@ -0,0 +1,7 @@
+const fontTag = document.createElement('link');
+
+fontTag.rel = 'stylesheet';
+fontTag.type = 'text/css';
+fontTag.href = 'https://fonts.googleapis.com/css?family=Saira+Extra+Condensed';
+
+document.getElementsByTagName('head')[0].appendChild(fontTag);
